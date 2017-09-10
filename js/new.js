9e7449b118c4ab75bbe2772a53e6433a53af0398
bi502 <hevnex@gmail.com>
2017-09-10 13:16:15 +0600

    Таблица умножения

--- js/new.js
+++ js/new.js
@@ -1,7 +1,12 @@
-sum = 0;
-for (var i=1; i<=50; i++) {
-    var a = i.split('').reverse().join('');
-    if ( a !== i)
-   // continue;
-    document.write(a+'<br>');
+document.write("<table border='1'>");
+for(var j=1; j<=10; j++){
+    document.write("<tr>");
+    for(var i=1; i<=10; i++){
+        if((i === 1)|| (j === 1)) {
+            document.write("<td bgcolor='#87cefa'>" + i * j + "</td>");
+        } else
+            document.write("<td >"+ i*j+ "</td>");
+    }
+    document.write('</tr>');
 }
+document.write("</table>");
